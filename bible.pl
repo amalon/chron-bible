41919930072b431b1df9e7b593ee60ad30f2c600
James Hogan <james@albanarts.com>
2013-07-27 20:14:45 +0100

    Initial Bible stub
    Just a copyright header to start with.
    
    Signed-off-by: James Hogan <james@albanarts.com>

---
+++ bible.pl
@@ -0,0 +1,22 @@
+/*
+ * The Bible
+ *
+ * Copyright (C) 2012-2013 James Hogan <james@albanarts.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
+ * (in the file called COPYING).
+ *
+ *
+ * Chron facts defining events, periods, geneologies, and chronological
+ * constraints for the Bible.
+ *
+ */
+
+event(epoch).
