4861667df4ea73d941c54aa526ae86eb5b737889
James Hogan <james@albanarts.com>
2013-08-08 00:38:46 +0100

    Genesis 12: famine caused Abram and Lot to go to Egypt
    Add the move to Egypt due to famine of Abram and Lot.
    
    Signed-off-by: James Hogan <james@albanarts.com>

--- genesis/12.pl
+++ genesis/12.pl
@@ -24,3 +24,5 @@ people_group(abram_lot, [abram, lot, sarai]).
 event_interval(birth(abram), move(abram, haran, 1, canaan, 1), genesis_years(75), bible([genesis, 12, 4])).
 people_moved(group(abram_lot), haran, 1, canaan, 1, bible([genesis, 12, 5])).
 
+% Famine, so Abram went to Egypt
+people_moved(group(abram_lot), canaan, 1, egypt, 1, bible([genesis, 12, 10])).
