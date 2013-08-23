564e642f86734f7585622d8e435d164eaabde64d
James Hogan <james@albanarts.com>
2013-08-23 09:05:41 +0100

    Genesis 29: fill in some details
    Jacob reached Padan-aram, stayed there for a month, then started working
    7 years for Rachel. Laban tricked him and gave him Leah as a wife
    instead. He then married Rachel too after a week, and worked another 7
    years.
    
    Leah then had 4 children.
    
    Signed-off-by: James Hogan <james@albanarts.com>

--- genesis/29.pl
+++ genesis/29.pl
@@ -19,10 +19,59 @@
  *
  */
 
-% Laban had two daughters, Leah was older, Rachel was younger.
+% Jacob reached Laban
+moved(jacob, beth_el, 1, padan_aram, 1, bible([genesis, 29, 1])).
+
+% Laban had two daughters, Leah was older, Rachel was younger
 parent_daughter(laban, leah, bible([genesis, 29, 16])).
 parent_daughter(laban, rachel, bible([genesis, 29, 16])).
+age_ordering([leah, rachel], bible([genesis, 29, 16])).
 
-% Jacob married both of them.
-married(jacob, leah, bible([genesis, 29])).
-married(jacob, rachel, bible([genesis, 29])).
+% After a month, Jacob served Laban 7 years for Rachel
+period(serve_for(jacob, laban, rachel)).
+event_interval(begin(dwell(jacob, padan_aram, 1)), begin(serve_for(jacob, laban, rachel)),
+		time(Days, day), bible([genesis, 29, 14])) :-
+	Days in 28..31.
+period_len(serve_for(jacob, laban, rachel), time(7, year), bible([genesis, 29, 20])).
+
+% Jacob married Leah
+married(jacob, leah, bible([genesis, 29, 23])).
+events_coincide([end(serve_for(jacob, laban, rachel)), begin(marriage(jacob, leah))],
+		bible([genesis, 29, 21])).
+
+% Zilpah given as Leah's handmaid
+woman(zilpah).
+
+% Jacob married Rachel too.
+married(jacob, rachel, bible([genesis, 29, 28])).
+event_interval(begin(marriage(jacob, leah)), begin(marriage(jacob, rachel)),
+		time(7, day), bible([genesis, 29, 28])).
+
+% Bilhah given as Rachel's handmaid
+woman(bilhah).
+
+% Jacob served Laban another 7 years for Leah
+period(serve_for(jacob, laban, leah)).
+period_len(serve_for(jacob, laban, leah), time(7, year), bible([genesis, 29, 30])).
+events_coincide([begin(marriage(jacob, rachel)), begin(serve_for(jacob, laban, leah))],
+		bible([genesis, 29, 30])).
+period_during(serve_for(jacob, laban, leah), dwell(jacob, padan_aram, 1),
+		bible([genesis, 29, 30])).
+
+
+% Leah bore Reuben after both married
+parents_son([jacob, leah], reuben, bible([genesis, 29, 32])).
+events_ordered([begin(marriage(jacob, rachel)), conception(reuben)],
+		bible([genesis, 29, 31])).
+
+% Leah then bore Simeon
+parents_son([jacob, leah], simeon, bible([genesis, 29, 33])).
+age_ordering([reuben, simeon], bible([genesis, 29, 33])).
+
+% Leah then bore Levi
+parents_son([jacob, leah], levi, bible([genesis, 29, 34])).
+age_ordering([simeon, levi], bible([genesis, 29, 34])).
+
+% Leah then bore Judah
+parents_son([jacob, leah], judah, bible([genesis, 29, 35])).
+age_ordering([levi, judah], bible([genesis, 29, 35])).
